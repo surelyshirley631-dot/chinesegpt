cfcd83c16ede451cdbce2ec94f91f2109c188eaa
Shirley Lau <shirleylau@Mac.fritz.box>
2026-02-12 14:25:17 +0100

    chore: remove unsupported eslint config from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
